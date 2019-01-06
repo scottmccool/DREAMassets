2a084e290b66627c59775545b0106ba2a0cfb509
Scott McCool <scott@mccool.com>
2019-01-05 18:56:40 -0800

    Begin a rewrite of the hub pipeline

---
+++ lucid/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='DREAM',
+      version='0.0.2',
+      python_requires='>=3.5',
+      description='Dream bluetooth framework, simplified',
+      url='https://github.com/DREAMassets-org/DREAMassets/',
+      author='DREAM',
+      author_email='no-reply@localhost',
+      license='MIT',
+      packages=['dream'],
+      zip_safe=False)
