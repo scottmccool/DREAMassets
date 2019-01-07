590a2e27f93c57f0cd43b2d6231f897c5333bcac
Scott McCool <scott@mccool.com>
2019-01-06 23:26:06 -0800

    cleanup rewrite attempt

--- lucid/dream/dreamconfig.py
+++
@@ -1,6 +0,0 @@
-import os
-GOOGLE_PROJECT_ID = os.environ.get("GOOGLE_PROJECT_ID", "dream-assets-project")
-GOOGLE_PUBSUB_TOPIC = os.environ.get("GOOGLE_PUBSUB_TOPIC ", "batched-payloads")
-BATCH_SIZE = os.environ.get("CLOUD_BATCH_SIZE", "10")
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "/usr/local/dream/sobun/config/google-credentials.secret.json")
-BTLE_TIMEOUT = os.environ.get("BTLE_TIMEOUT","20.0")
--- lucid/original_dream/__init__.py
+++
@@ -1,1 +0,0 @@
-# this under under (__init__) file is to make this folder a python package
