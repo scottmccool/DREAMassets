4953078ca4a099f76b10748d496679a0364a1272
Scott McCool <scott@mccool.com>
2019-01-08 19:31:36 -0800

    Cleanup pubsub logic

---
+++ lucid/dreamhub/publisher/gpub.py
@@ -0,0 +1,31 @@
+import socket
+import time
+import json
+import base64
+import os
+
+from dreamhub import dreamconfig
+
+from google.cloud import pubsub_v1
+
+# During setup, we set the RasPi's hostname to the Hub ID  
+HUB_ID = socket.gethostname()
+
+# This replaces sobun publish method and conforms better to google cloud api docs
+# We send the observation as a utf-8 encoded string and set attributes as appropriate
+# @todo add batching logic
+def sendit(observation):
+    """ Publish a filtered observation to the cloud """
+    try:
+        observation = json.dumps(observation).encode('utf-8')
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(dreamconfig.GOOGLE_PROJECT_ID, dreamconfig.GOOGLE_PUBSUB_TOPIC)
+        publisher.publish(topic_path, 
+            data=observation,
+            hub_id=HUB_ID,
+            hub_publisher_version=dreamconfig.VERSION,
+            hub_timestamp = str(time.time())
+        )
+    except Exception as e:
+        print("Unable to publish data to Google Cloud due to error: {}".format(e))
+        raise
--- lucid/dreamhub/publisher/tasks.py
+++ lucid/dreamhub/publisher/tasks.py
@@ -2,22 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 import os
-import socket
-import time
-import json
-from google.oauth2 import service_account
-import googleapiclient.discovery
-import base64
-
-from dreamhub import dreamconfig
-
-# During setup, we set the RasPi's hostname to the Hub ID  
-HUB_ID = socket.gethostname()
-SCOPES = ['https://www.googleapis.com/auth/pubsub']
-# The Hub publishes to this topic on PubSub 
-TOPIC = "projects/{}/topics/{}".format(dreamconfig.GOOGLE_PROJECT_ID, dreamconfig.GOOGLE_PUBSUB_TOPIC)
-SERVICE_ACCOUNT_FILE = dreamconfig.GOOGLE_APPLICATION_CREDENTIALS
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+from dreamhub.publisher import gpub
 
 #@app.task(base=Batches,flush_every=100, flush_interval=60)
 @shared_task
@@ -27,33 +12,4 @@ def publish(payload):
     f.write(p)
     f.write("\n")
     f.close()
-    
-
-@shared_task(rate_limit=1, timeout=5)
-def publish_to_google(payload):
-    # @TODO clean up encoding!
-    try:
-        enc = base64.b64encode(json.dumps(payload).encode('utf-8'))
-    except Exception as e:
-        print("Couldn't encode payload (type: %s, error: %s)" % (type(payload),e))
-        raise
-    message = {
-        "messages": [
-            {
-                "data": enc,
-                "attributes": {
-                    "hub_id": HUB_ID,
-                    "hub_publisher_version": dreamconfig.VERSION,
-                    "hub_timestamp": time.time()
-                }
-            }
-        ]
-    }
-    try:
-        pubsub = googleapiclient.discovery.build('pubsub', 'v1', credentials=credentials)
-        res = pubsub.projects().topics().publish(topic=TOPIC, body=message).execute()
-        return res
-    except Exception as e:
-        print("Unable to publish data to Google Cloud due to network error: {}".format(e))
-        raise
-
+    gpub.sendit(payload)
